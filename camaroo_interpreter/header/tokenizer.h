24fd2a860c1e804c10b5723c2546cea154818cde
PavlyDev <bavleydanial@gmail.com>
2024-09-03 22:44:17 +1000

    Some refactoring

--- camaroo_interpreter/header/tokenizer.h
+++ camaroo_interpreter/header/tokenizer.h
@@ -1,43 +1,46 @@
-#include<iostream>
-namespace camaroo_core 
-{
-    enum class TokenType {
-        unknown = 0,
-        num8, num16, num32, num64,
-        fnum32, fnum64,
-        letter, text,
-        toggle,
-        func,
-        add, subtract, multiply, division, equal,
-        LParen, RParen,
-        semicolon,
-        equal_conditional, and_conditional, or_conditional, not_conditional,
-        //variable names:
-        num8_type,num16_type,num32_type,num64_type,
-        fnum32_type,fnum64_type,
-        text_type,letter_type,
-        toggle_type,
-        func_type,
-    };
-    
-    struct Token
-    {
-        TokenType Token;
-        std::string value;
-    };
+#include <string>
+#include <optional>
 
-    class Tokenizer{
-        Tokenizer(const std::string& text);
-        void advance();
-        Token next_token();
-        private:
-            std::string get_number();
-            Token return_num_token(const std::string& result);
-            Token return_fnum_token(const std::string& result);
-            TokenType check_std_type(const std::string& result);
-        private:
-            std::string text;
-            size_t pos;
-            char current_char;
-    };
+namespace camaroo_core {
+	enum class TokenType {
+		unknown = 0,
+		// data values
+		num8, num16, num32, num64,
+		fnum32, fnum64,
+		letter, text,
+		toggle,
+		// math operators
+		add, subtract, multiply, division, equal,
+		equal_conditional, and_conditional, or_conditional, not_conditional,
+		// other
+		LParen, RParen,
+		semicolon,
+		// data types
+		num8_type,num16_type,num32_type,num64_type,
+		fnum32_type,fnum64_type,
+		text_type,letter_type,
+		toggle_type,
+		func_type,
+	};
+
+	struct Token {
+		TokenType Token;
+		std::string value;
+	};
+
+	class Tokenizer {
+		public:
+			Tokenizer(const std::string& text);
+			void advance();
+			std::optional<Token> next_token();
+		private:
+			std::string get_number();
+			Token return_num_token(const std::string& result);
+			Token return_fnum_token(const std::string& result);
+			TokenType check_std_type(const std::string& result);
+		private:
+			std::string text;
+			size_t pos;
+			char current_char;
+	};
 }
--- camaroo_interpreter/src/tokenizer.cpp
+++ camaroo_interpreter/src/tokenizer.cpp
@@ -1,187 +1,184 @@
-#include <Tokenizer.h>
-#include <iostream>
+#include <tokenizer.h>
+#include <limits.h>
 #include <float.h>
-namespace camaroo_core{
 
+namespace camaroo_core {
 
-    Tokenizer::Tokenizer(const std::string& text): text(text), pos(0){
-        current_char = text[pos];
-    }
-    void Tokenizer::advance(){
-        ++pos;
-        if(pos >= text.length()){
-            current_char = '\0';
-        }else{
-            current_char = text[pos];
-        }
-    }
+	Tokenizer::Tokenizer(const std::string& text)
+		:text(text), pos(0) {
+			current_char = text[pos];
+	}
 
-    std::string Tokenizer::get_number(){
-        std::string result;
-        while(isdigit(current_char)){
-            result+= current_char;
-            advance();
-        }
-    }
+	void Tokenizer::advance() {
+		++pos;
+		if (pos >= text.length()) {
+			current_char = '\0';
+		} else {
+			current_char = text[pos];
+		}
+	}
 
-    Token Tokenizer::return_num_token(const std::string& result){
-            if(std::stoll(result) > INT_MAX){
-                return(Token{TokenType::num64, result});
-            }else if(std::stoi(result) > SHRT_MAX){
-                return(Token{TokenType::num32, result});
-            }else if(std::stoi(result) > CHAR_MAX){
-                return(Token{TokenType::num16, result});
-            }else{
-                return(Token{TokenType::num8, result});
-            }
-    }
+	std::string Tokenizer::get_number() {
+		std::string result;
+		while (isdigit(current_char)) {
+			result += current_char;
+			advance();
+		}
+		return result;
+	}
 
-    Token Tokenizer::return_fnum_token(const std::string& result){
-            if(std::stold(result) > FLT_MAX){
-                return(Token{TokenType::fnum64, result});
-            }else{
-                return(Token{TokenType::fnum32, result});
-            }
-    }
+	Token Tokenizer::return_num_token(const std::string& result) {
+		if (std::stoll(result) > INT_MAX) {
+			return(Token{TokenType::num64, result});
+		} else if (std::stoi(result) > SHRT_MAX) {
+			return(Token{TokenType::num32, result});
+		} else if (std::stoi(result) > CHAR_MAX) {
+			return(Token{TokenType::num16, result});
+		} else {
+			return(Token{TokenType::num8, result});
+		}
+	}
 
-    TokenType Tokenizer::check_std_type(const std::string& result){
-            if(result == "num8"){
-                return TokenType::num8_type;
-            }else if(result == "num16"){
-                return TokenType::num16_type;
-            }else if(result == "num32"){
-                return TokenType::num32_type;
-            }else if(result == "num64"){
-                return TokenType::num64_type;
-            }else if(result == "fnum32"){
-                return TokenType::fnum32_type;
-            }else if(result == "fnum64"){
-                return TokenType::fnum64_type;
-            }else if(result == "text"){
-                return TokenType::text_type;
-            }else if(result == "letter"){
-                return TokenType::letter_type;
-            }else if(result == "func"){
-                return TokenType::func_type;
-            }else if(result == "toggle"){
-                return TokenType::toggle_type;
-            }else{
-                return TokenType::unknown;
-            }
-    }
+	Token Tokenizer::return_fnum_token(const std::string& result) {
+		if (std::stold(result) > FLT_MAX) {
+			return(Token{TokenType::fnum64, result});
+		} else {
+			return(Token{TokenType::fnum32, result});
+		}
+	}
 
-    Token Tokenizer::next_token(){
-        while (current_char!= '\0')
-        {
-            if(isdigit(current_char)){
-                std::string result = get_number();
-                if(std::stoll(result) == std::stold(result)){
-                    return return_num_token(result);
-                }
-                else{
-                    return return_fnum_token(result);
-                }
-            }
+	TokenType Tokenizer::check_std_type(const std::string& result) {
+		if (result == "num8") {
+			return TokenType::num8_type;
+		} else if (result == "num16") {
+			return TokenType::num16_type;
+		} else if (result == "num32") {
+			return TokenType::num32_type;
+		} else if (result == "num64") {
+			return TokenType::num64_type;
+		} else if (result == "fnum32") {
+			return TokenType::fnum32_type;
+		} else if (result == "fnum64") {
+			return TokenType::fnum64_type;
+		} else if (result == "text") {
+			return TokenType::text_type;
+		} else if (result == "letter") {
+			return TokenType::letter_type;
+		} else if (result == "func") {
+			return TokenType::func_type;
+		} else if (result == "toggle") {
+			return TokenType::toggle_type;
+		} else {
+			return TokenType::unknown;
+		}
+	}
 
-            if(current_char == '#'){
-                advance();
-                if(current_char=='#'){
-                    while(current_char != '#'){
-                        advance();
-                    }
-                    advance();
-                    advance();
-                }
-            }
+	std::optional<Token> Tokenizer::next_token() {
+		while (current_char!= '\0') {
+			if (isdigit(current_char)) {
+				std::string result = get_number();
+				if (std::stoll(result) == std::stold(result)) {
+					return return_num_token(result);
+				}
+				else {
+					return return_fnum_token(result);
+				}
+			}
 
-            if(current_char == '/'){
-                advance();
-                if(current_char == '/'){
-                    while(current_char != '\n'){
-                        advance();
-                    }
-                }
-            }
+			if (current_char == '#') {
+				advance();
+				if(current_char=='#') {
+					while (current_char != '#') {
+						advance();
+					}
+					advance();
+					advance();
+				}
+			}
 
-            if(current_char == '\''){
-                std::string result = "\'";
-                advance();
-                if(current_char == '\\'){
-                    result+= current_char;
-                    advance();
-                }
-                result+=current_char;
-                advance();
-                result+=current_char;
-                advance();
-                return(Token{TokenType::letter, result});
-            }
+			if (current_char == '/') {
+				advance();
+				if (current_char == '/') {
+					while (current_char != '\n') {
+						advance();
+					}
+				}
+			}
 
-            if(current_char == '\"'){
-                std::string result = "\"";
-                advance();
-                while (true)
-                {
-                    if(current_char == '\"' && result[result.size()-1] != '\\'){
-                        result+= current_char;
-                        advance();
-                        break;
-                    }
-                    result+= current_char;
-                    advance();
-                }
-                return(Token{TokenType::text, result});
-            }
+			if (current_char == '\'') {
+				std::string result = "\'";
+				advance();
+				if (current_char == '\\') {
+					result += current_char;
+					advance();
+				}
+				result += current_char;
+				advance();
+				result += current_char;
+				advance();
+				return(Token{TokenType::letter, result});
+			}
 
-            if((current_char >= 'a' && current_char <= 'z') || (current_char >= 'A' && current_char <= 'Z')){
-                std::string result = "";
-                result += current_char;
-                advance();
-                while (current_char != ' ')
-                {
-                    result += current_char;
-                    advance();
-                }
-                TokenType token_type = check_std_type(result);
-                return(Token{token_type, result});
-            }
+			if (current_char == '\"') {
+				std::string result = "\"";
+				advance();
+				while (true) {
+					if (current_char == '\"' && result[result.size()-1] != '\\') {
+						result += current_char;
+						advance();
+						break;
+					}
+					result += current_char;
+					advance();
+				}
+				return(Token{TokenType::text, result});
+			}
 
-            if(current_char == '+'){
-                advance();
-                return(Token{TokenType::add, std::string("+")});
-            }
-            if(current_char == '-'){
-                advance();
-                return(Token{TokenType::subtract, std::string("-")});
-            }
-            if(current_char == '*'){
-                advance();
-                return(Token{TokenType::multiply, std::string("*")});
-            }
-            if(current_char == '/'){
-                advance();
-                return(Token{TokenType::division, std::string("/")});
-            }
-            if(current_char == '('){
-                advance();
-                return(Token{TokenType::LParen, std::string("(")});
-            }
-            if(current_char == ')'){
-                advance();
-                return(Token{TokenType::RParen, std::string(")")});
-            }
-            if(current_char == ';'){
-                advance();
-                return(Token{TokenType::semicolon, std::string(";")});
-            }
-            if(current_char == '='){
-                advance();
-                return(Token{TokenType::equal, std::string("=")});
-            }
-            if(current_char == '=='){
-                advance();
-                return(Token{TokenType::equal_conditional, std::string("==")});
-            }
-        }
-    }
+			if ((current_char >= 'a' && current_char <= 'z') || (current_char >= 'A' && current_char <= 'Z')) {
+				std::string result = "";
+				result += current_char;
+				advance();
+				while (current_char != ' ') {
+					result += current_char;
+					advance();
+				}
+				TokenType token_type = check_std_type(result);
+				return(Token{token_type, result});
+			}
+
+			if (current_char == '+') {
+				advance();
+				return(Token{TokenType::add, std::string("+")});
+			}
+			if (current_char == '-') {
+				advance();
+				return(Token{TokenType::subtract, std::string("-")});
+			}
+			if (current_char == '*') {
+				advance();
+				return(Token{TokenType::multiply, std::string("*")});
+			}
+			if (current_char == '/') {
+				advance();
+				return(Token{TokenType::division, std::string("/")});
+			}
+			if (current_char == '(') {
+				advance();
+				return(Token{TokenType::LParen, std::string("(")});
+			}
+			if (current_char == ')') {
+				advance();
+				return(Token{TokenType::RParen, std::string(")")});
+			}
+			if (current_char == ';') {
+				advance();
+				return(Token{TokenType::semicolon, std::string(";")});
+			}
+			if (current_char == '=') {
+				advance();
+				return(Token{TokenType::equal, std::string("=")});
+			}
+		}
+		return std::nullopt;
+	}
 }
